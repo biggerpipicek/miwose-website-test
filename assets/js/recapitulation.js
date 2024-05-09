0db0206dc5d3f803aa00d7eb00e0a2bd0ad7bbb2
Michael <96359292+biggerpipicek@users.noreply.github.com>
2024-05-09 19:31:13 +0200

    gg

--- assets/js/recapitulation.js
+++ assets/js/recapitulation.js
@@ -1,9 +1,19 @@
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
-const productsWrapper = document.querySelector('.lg');
-productsWrapper.innerHTML = ''; // Clear existing cart display
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+const productsWrapper = document.querySelector(".lg");
+productsWrapper.innerHTML = "";
 
 cart.forEach(product => {
     const productHTML = `
         <li class="list-group-item"><b>${product.productName}</b> - ${product.quantity} ks - ${product.productPrice * product.quantity} Kč</li>`;
     productsWrapper.innerHTML += productHTML;
+});
+
+let coupon = JSON.parse(localStorage.getItem("COUPON_APPLIED")) || [];
+const productsWrapper = document.querySelector(".lg");
+productsWrapper.innerHTML = "";
+
+cart.forEach(product => {
+    const couponHTML = `
+        <li class="list-group-item"><b>${coupon.name}</b> - ${coupon.percentage} %</li>`;
+    productsWrapper.innerHTML += couponHTML;
 });
