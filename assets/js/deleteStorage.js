77cb34209cab8667c8d742591414d55c43124e22
Michael <96359292+biggerpipicek@users.noreply.github.com>
2024-05-10 22:51:45 +0200

    ff

--- assets/js/deleteStorage.js
+++ assets/js/deleteStorage.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (window.location.pathname === "uspesna_platba.html") {
-        // Remove localStorage items
-        localStorage.removeItem("cart");
-        localStorage.removeItem("user_data");
-        
-        if (localStorage.getItem("COUPON_APPLIED")) {
-            localStorage.removeItem("COUPON_APPLIED");
-            localStorage.setItem("COUPON_APPLIED_FLAG", "false");
-        }
-    }
-});
+localStorage.removeItem("cart");
+localStorage.removeItem("user_data");
+localStorage.removeItem("COUPON_APPLIED");
+localStorage.setItem("COUPON_APPLIED_FLAG", "false");
